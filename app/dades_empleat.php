82312d8d9228d47a6bd0df63ae963602df82ca70
Ferran <ferrancliment2@iesmontsia.org>
2019-01-25 15:53:31 +0100

    EMPLEATS CASI LLISTAR

--- app/dades_empleat.php
+++ app/dades_empleat.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class dades_empleat extends Model
 {
     //
+    protected $table = 'dades_empleats';
+
+    protected $fillable = [
+        'titol',
+        'descripcio',
+        'prioritat',
+        'id_estat',
+        'id_usuari_client',
+        'id_usuari_empleat'
+    ];
 }

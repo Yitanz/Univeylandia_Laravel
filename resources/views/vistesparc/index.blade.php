667a2b38883cedbf35f9504fc8cdea3f64805bdf
Evaldas <evaldas_casas@hotmail.com>
2019-01-31 21:14:32 +0100

    ay lmao2

--- resources/views/vistesparc/index.blade.php
+++ resources/views/vistesparc/index.blade.php
@@ -6,7 +6,7 @@
 @endsection
 @section("body")
 <!-- SLIDER-->
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div id="carousel" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100" src="/img/slider1.jpg" alt="First slide">
@@ -18,11 +18,11 @@
       <img class="d-block w-100" src="/img/slider3.jpg" alt="Third slide">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
